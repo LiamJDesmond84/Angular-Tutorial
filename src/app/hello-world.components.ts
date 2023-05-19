b68c9bb07e9a0d9a799cc612512eef01b5fca67d
Liam Desmond <liamjdesmond84@gmail.com>
2023-05-19 14:50:51 -0500

    Importing first component

--- src/app/hello-world.components.ts
+++ src/app/hello-world.components.ts
@@ -2,7 +2,7 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-hello-world',
-    template: '',
+    template: '<h1>{{title}}</h1>',
     styles: [`
     h1 {
         color: blue;
