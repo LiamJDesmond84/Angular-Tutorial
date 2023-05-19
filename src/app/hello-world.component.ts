d8767bfdf2afb2bb89eb9263910be99167950268
Liam Desmond <liamjdesmond84@gmail.com>
2023-05-19 16:23:09 -0500

    Messing with componenets

--- src/app/hello-world.component.ts
+++ src/app/hello-world.component.ts
@@ -2,8 +2,7 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-hello-world',
-    templateUrl: './hello-world.component.html',
-    // template: '<h1>{{title}} - {{otherThing}}</h1>',
+    template: '<h1>{{title}} - {{otherThing}}</h1>',
     styles: [`
     h1 {
         color: red;
