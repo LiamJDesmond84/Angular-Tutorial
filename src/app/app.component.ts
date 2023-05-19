1920bd817bf6f262822ca82b80bddb79e95aaf80
Liam Desmond <liamjdesmond84@gmail.com>
2023-05-19 15:11:41 -0500

    Adding OnDestroy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
+
   title = 'Angular-Tutorial';
 
-  intervalSub;
+  intervalSub: string | number | NodeJS.Timer | undefined;
 
   ngOnInit() {
     this.intervalSub = setInterval(() => {
       console.log("Heyyy from OnInit");
-    });
+    }, 1000);
+  }
+
+  ngOnDestroy() {
+    if(this.intervalSub){
+      clearInterval(this.intervalSub);
+    }
   }
 }
