8d8e70098a5125c95a4413991eb8477e4a25f1a1
Liam Desmond <liamjdesmond84@gmail.com>
2023-05-19 14:48:01 -0500

    Adding CSS

--- src/app/hello-world.components.ts
+++ src/app/hello-world.components.ts
@@ -3,7 +3,13 @@ import { Component } from "@angular/core";
 @Component({
     selector: 'app-hello-world',
     template: '',
-    styles: [``]
+    styles: [`
+    h1 {
+        color: blue;
+    }`]
 })
 
-export class HelloWorldComponent {}
+export class HelloWorldComponent {
+    title = "Hello";
+
+}
