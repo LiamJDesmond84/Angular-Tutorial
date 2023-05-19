3c68f7745e539f7616354230916d362fb002a768
Liam Desmond <liamjdesmond84@gmail.com>
2023-05-19 15:12:20 -0500

    Adding OnDestroy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,6 +19,7 @@ export class AppComponent implements OnInit, OnDestroy {
 
   ngOnDestroy() {
     if(this.intervalSub){
+      console.log("AYYY from OnDestroy");
       clearInterval(this.intervalSub);
     }
   }
