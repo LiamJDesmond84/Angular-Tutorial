921a41ad3fe3e3818756810fb38082b2fa0ac938
Liam Desmond <liamjdesmond84@gmail.com>
2023-05-20 11:46:36 -0500

    Generating new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HelloWorldComponent } from './hello-world.component';
 import { ParentComponent } from './parent/parent.component';
 import { ChildComponent } from './child/child.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HelloWorldComponent,
     ParentComponent,
-    ChildComponent
+    ChildComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
