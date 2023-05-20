29926c3fde61477eec16d146f72b5e2164a21040
Liam Desmond <liamjdesmond84@gmail.com>
2023-05-19 21:39:24 -0500

    Moving stuff around

--- src/app/hello-world.component.ts
+++ src/app/hello-world.component.ts
@@ -2,7 +2,8 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-hello-world',
-    template: '<h1>{{title}} - {{otherThing}}</h1>',
+    // template: '<h1>{{title}} - {{otherThing}}</h1>',
+    templateUrl: './hello-world.component.html',
     styles: [`
     h1 {
         color: red;
