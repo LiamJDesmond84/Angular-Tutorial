5f131e41632cad49718989a778f1f0be92014c4f
Liam Desmond <liamjdesmond84@gmail.com>
2023-05-20 12:03:23 -0500

    Messing with stuff

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -8,4 +8,8 @@ import { Component, Input } from '@angular/core';
 export class ButtonComponent {
   @Input() text: string;
   @Input() color: string;
+
+  clickMe(){
+    console.log("Add");
+  }
 }
