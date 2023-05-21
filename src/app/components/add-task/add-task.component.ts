5407290903d0b3cfab1292a43dce96001817cb24
Liam Desmond <liamjdesmond84@gmail.com>
2023-05-21 13:31:33 -0500

    Added addTask service method

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
+import { Task } from 'src/app/Task';
 import { TaskServiceService } from 'src/app/services/task-service.service';
 
 @Component({
@@ -13,6 +14,8 @@ export class AddTaskComponent {
   text: string;
   day: string;
   reminder: boolean = false;
+
+  @Output() submitTask: EventEmitter<Task> = new EventEmitter();
 
   onSubmit(){
 
@@ -29,9 +32,13 @@ export class AddTaskComponent {
       reminder: this.reminder
     }
 
-    this.taskService.addNewTask(newTask).subscribe();
+    // this.taskService.addNewTask(newTask).subscribe();
 
+    this.submitTask.emit(newTask);
 
+    this.text = "";
+    this.day = "";
+    this.reminder = false;
   }
 
 }
